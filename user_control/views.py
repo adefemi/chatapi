7547f1cb1e9f892a9133c15243c567a02fbee5aa
Adefemi Oseni <adefemi.oseni@algorismng.com>
2020-11-21 16:18:43 +0100

    updated

--- user_control/views.py
+++ user_control/views.py
@@ -123,6 +123,7 @@ class UserProfileView(ModelViewSet):
             return self.queryset
 
         data = self.request.query_params.dict()
+        data.pop("page", None)
         keyword = data.pop("keyword", None)
 
         if keyword:
